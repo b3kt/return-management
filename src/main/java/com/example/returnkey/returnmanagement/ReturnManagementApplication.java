d6eb93d045b856e376349db354f045ca816dee45
Subekti Cahyowibowo <subekti.cahyo@gmail.com>
2022-02-09 17:39:31 +0700

    initial commit create starter app for springboot application

---
+++ src/main/java/com/example/returnkey/returnmanagement/ReturnManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.returnkey.returnmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/returnkey/returnmanagement/ReturnManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.returnkey.returnmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
